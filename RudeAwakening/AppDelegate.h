d434a2750fc70f73efea2e0a1d4922312ecf3894
Alex French <frenchal17@grinnell.edu>
2017-01-04 21:27:11 -0600

    Initial Commit

---
+++ RudeAwakening/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  RudeAwakening
+//
+//  Created by Alex French on 1/4/17.
+//  Copyright © 2017 Frenchal. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ RudeAwakening/ViewController.m
@@ -0,0 +1,29 @@
+//
+//  ViewController.m
+//  RudeAwakening
+//
+//  Created by Alex French on 1/4/17.
+//  Copyright © 2017 Frenchal. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+
+@end
